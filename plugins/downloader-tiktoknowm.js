98a6d2a1fa7895f2bb0228242d603cb25eaa346a
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-24 23:11:15 +0700

    Update downloader-tiktoknowm.js

--- plugins/downloader-tiktoknowm.js
+++ plugins/downloader-tiktoknowm.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix, command, args }) => {
    let res = await fetch(`https://api.ibeng.tech/api/download/tiktok?url=${args[0]}&apikey=ibeng`)
   if (!res.ok) throw await res.text()
 let json = await res.json()
-   let cap = `${ucapan()}\n\nNih ${name}\nCara Simpan\n\n1. Download Dulu Vidio Nya Kalau Sudah\n2. Masuk Ke Dalam Vidio Nya\n3. Lalu Tekan Titik3 Pojok Kanan Atas\n4. Lalu Pilih Yang Ada Bacaan  Simpan `
+   let cap = `Cara Simpan\n\n1. Download Dulu Vidio Nya Kalau Sudah\n2. Masuk Ke Dalam Vidio Nya\n3. Lalu Tekan Titik3 Pojok Kanan Atas\n4. Lalu Pilih Yang Ada Bacaan  Simpan `
 conn.sendFile(m.chat, json.result.video, 'tiktok.mp4', cap, m)
 }
 handler.help = ['ttnowm']
