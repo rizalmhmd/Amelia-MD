4e4c587a4646524abfd7f9ba58c516e36c5015be
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-24 21:58:24 +0700

    Update downloader-tiktoknowm.js

--- plugins/downloader-tiktoknowm.js
+++ plugins/downloader-tiktoknowm.js
@@ -2,10 +2,11 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command, args }) => {
    if (!args[0]) return m.reply(`Masukan Url TikTok !`)
-   let rest = await fetch(`https://malesin.xyz/tiktok?url=${args[0]}`)
+   let rest = await fetch(`https://api.ibeng.tech/api/download/tiktok?url=${args[0]}&apikey=ibeng`)
    let res = await rest.json()
    let { title, author, video, audio, videoWM } = res
-   let kon = `${title}
+   let kon = `*Nama* : ${username}
+   *Description* : ${description}
 
 Author : ${author}
 
@@ -13,9 +14,9 @@ Powered by © #${wm}`
 m.reply(wait)
 await conn.sendFile(m.chat, video, 'tt.mp4', kon, m)
 }
-handler.help = ['tiktoknowm', 'ttnowm']
+handler.help = ['ttnowm']
 handler.tags = ['downloader']
-handler.command = /^(ttnowm|tiktoknowm)$/i
+handler.command = /^(ttnowm)$/i
 
 handler.premium = false
 handler.limit = true 
